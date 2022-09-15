a08cdb967e2449be3b801b6ab6d95468f0203bdb
unknown <light2009@sookmyung.ac.kr>
2022-09-15 15:18:36 +0900

    간단한 투두리스트

--- lib/secondDetail.dart
+++ lib/secondDetail.dart
@@ -3,19 +3,29 @@ import 'package:flutter/material.dart';
 class SecondDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = new TextEditingController();
     return Scaffold(
         appBar: AppBar(
           title: Text('Second Page'),
         ),
         body: Container(
             child: Center(
-                child: ElevatedButton(
-                  onPressed: (){
-                    Navigator.of(context).pushReplacementNamed('/third');
-                    //Navigator의 pop은 스택에서 제일 위에 있는 페이지를 제거
-                  },
-                  child: Text('세 번째 페이지로 이동하기'),
-                )
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: controller,
+                    keyboardType: TextInputType.text,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      Navigator.of(context).pop(controller.value.text); //이 값이 await 부분의 return 값으로 간다는 건가?
+
+                    },
+                    child: Text('저장하기'),
+                  )
+                ],
+              )
+
             )
         )
     );
--- lib/subDetail.dart
+++ lib/subDetail.dart
@@ -6,22 +6,54 @@ class SubDetail extends StatefulWidget {
 }
 
 class _SubDetail extends State<SubDetail> {
+  List<String> todoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    todoList.add('당근 사오기');
+    todoList.add('약 사오기');
+    todoList.add('청소하기');
+    todoList.add('부모님께 전화하기');
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sub Detail Example'),
       ),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).pushNamed('/second'); //pushReplacementName는 스택에 있는 자료를 교체한다
-            },
-            child: Text('두 번째 페이지로 이동하기'),
-          )
-        )
-      )
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell( //InkWell 위젯은 탭, 더블탭, 롱탭 등 다양한 터치 이벤트를 처리할 수 있다
+              child: Text(
+                todoList[index],
+                style: TextStyle(fontSize: 30),
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushNamed('/third', arguments: todoList[index]); //third 페이지로 이동하면서 arguments에는 어떤 데이터를 지정해서 그 페이지로 넘긴다
+              }
+            ),
+          );
+        },
+        itemCount: todoList.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addNavigation(context);
+        },
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  void _addNavigation(BuildContext context) async { //데이터를 받은 다음 처리해야 하니 비동기로 작성
+    final result = await Navigator.of(context).pushNamed('/second'); //await 뒤에 적힌 함수가 끝날 때까지 기다리고 반환값을 todolist에 추가
+    setState(() {
+      todoList.add(result.toString());
+    });
   }
 }
